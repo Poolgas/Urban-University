e3c17cb4cbd986d21e84667f4cbd93ed7deff3f4
Poolgas <Stealik174@gmail.com>
2024-05-11 01:59:52 +0500

    Add files via upload
    Практическая работа по уроку "Динамическая типизация"

---
+++ Homework2.py
@@ -0,0 +1,8 @@
+name = 'Kirill'
+print('Name: ' + name)
+age = 29
+print('Age: ' + str(age))
+age = (age + 1)
+print('New Age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
