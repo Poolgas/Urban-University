21e39f4b93d8eb7fefe3bf7eca4dd6745bced8a0
Poolgas <Stealik174@gmail.com>
2024-05-14 02:14:25 +0500

    Update Module1hard.py

--- Module1hard.py
+++ Module1hard.py
@@ -5,4 +5,5 @@ avg_grades = [sum(int(el) for el in i) for i in grades]
 len_grades = [len(i) for i in grades]
 for i in range(len(avg_grades)):
     avg_grades[i] = avg_grades[i] / len_grades[i]
-print(dict(zip(Students, avg_grades)))
+dictionary = dict(zip(Students, avg_grades))
+print(dictionary)
