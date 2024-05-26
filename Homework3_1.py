441c800272163cfff3a20a1a3f58b7e3ec23ab30
Poolgas <Stealik174@gmail.com>
2024-05-26 09:15:44 +0500

    Add files via upload
    Домашняя работа по уроку "Пространство имен и способы вызова функции"

---
+++ Homework3_1.py
@@ -0,0 +1,13 @@
+def single_root_words(root_words, *other_words):
+    same_words = []
+    for i in other_words:
+        i.lower()
+        root_words.lower()
+        if (i.lower()).count(root_words.lower()) == 1 or (root_words.lower()).count(i.lower()) == 1:
+            same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablemet', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
