f31d844bec01aa158b57c9cf905d146409e594de
Poolgas <Stealik174@gmail.com>
2024-07-18 00:02:36 +0500

    Update Homework7_5.py

--- Homework7_5.py
+++ Homework7_5.py
@@ -42,7 +42,7 @@ for root, dirs, files in os.walk(directory):
         filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
+        parent_dir = os.path.dirname(directory)
         print(
             f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория. {parent_dir}')
     break
