478aff18f04e136b39335d130a7854d3f72f75a0
Poolgas <Stealik174@gmail.com>
2024-07-19 09:41:28 +0500

    Update Homework8_2.py

--- Homework8_2.py
+++ Homework8_2.py
@@ -51,7 +51,7 @@ def personal_sum(numbers):
         except TypeError:
             incorrect_data += 1
             print(f'Некорректный тип данных для подсчёта суммы - {n}')
-    return (result, incorrect_data)
+    return result, incorrect_data
 
 
 def calculate_average(numbers):
