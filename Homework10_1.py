52e377207d55ef9982e9670e444f98ec76655135
Poolgas <Stealik174@gmail.com>
2024-08-09 19:20:26 +0500

    Домашнее задание по теме "Создание потоков".
    Цель: понять как работают потоки на практике, решив задачу
    
    Задача "Потоковая запись в файлы":

---
+++ Homework10_1.py
@@ -0,0 +1,89 @@
+'''Домашнее задание по теме "Создание потоков".
+
+Цель: понять как работают потоки на практике, решив задачу
+
+Задача "Потоковая запись в файлы":
+Необходимо создать функцию wite_words(word_count, file_name), где word_count - количество записываемых слов, file_name - название файла, куда будут записываться слова.
+Функция должна вести запись слов "Какое-то слово № <номер слова по порядку>" в соответствующий файл с прерыванием после записи каждого на 0.1 секунду.
+Сделать паузу можно при помощи функции sleep из модуля time, предварительно импортировав её: from time import sleep.
+В конце работы функции вывести строку "Завершилась запись в файл <название файла>".
+
+После создания файла вызовите 4 раза функцию wite_words, передав в неё следующие значения:
+10, example1.txt
+30, example2.txt
+200, example3.txt
+100, example4.txt
+После вызовов функций создайте 4 потока для вызова этой функции со следующими аргументами для функции:
+10, example5.txt
+30, example6.txt
+200, example7.txt
+100, example8.txt
+Запустите эти потоки методом start не забыв, сделать остановку основного потока при помощи join.
+Также измерьте время затраченное на выполнение функций и потоков. Как это сделать рассказано в лекции к домашнему заданию.
+
+Пример результата выполнения программы:
+Алгоритм работы кода:
+# Импорты необходимых модулей и функций
+# Объявление функции write_words
+# Взятие текущего времени
+# Запуск функций с аргументами из задачи
+# Взятие текущего времени
+# Вывод разницы начала и конца работы функций
+# Взятие текущего времени
+# Создание и запуск потоков с аргументами из задачи
+# Взятие текущего времени
+# Вывод разницы начала и конца работы потоков
+Вывод на консоль:
+Завершилась запись в файл example1.txt
+Завершилась запись в файл example2.txt
+Завершилась запись в файл example3.txt
+Завершилась запись в файл example4.txt
+Работа потоков 0:00:34.003411 # Может быть другое время
+Завершилась запись в файл example5.txt
+Завершилась запись в файл example6.txt
+Завершилась запись в файл example8.txt
+Завершилась запись в файл example7.txt
+Работа потоков 0:00:20.071575 # Может быть другое время
+'''
+
+from threading import Thread
+from datetime import datetime
+from time import sleep
+
+
+def wite_words(word_count, file_name):
+    with open(file_name, 'w') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+
+time_start_func = datetime.now()
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+time_end_func = datetime.now()
+res_func = time_end_func - time_start_func
+print(f"Работа функций {res_func}.")
+
+time_start_threads = datetime.now()
+thr_five = Thread(target=wite_words, args=(10, 'example5.'))
+thr_six = Thread(target=wite_words, args=(30, 'example6.'))
+thr_seven = Thread(target=wite_words, args=(200, 'example7.'))
+thr_eight = Thread(target=wite_words, args=(100, 'example8.'))
+
+thr_five.start()
+thr_six.start()
+thr_seven.start()
+thr_eight.start()
+
+thr_five.join()
+thr_six.join()
+thr_seven.join()
+thr_eight.join()
+
+time_end_threads = datetime.now()
+res_threads = time_end_threads - time_start_threads
+print(f"Работа потоков {res_threads}.")
