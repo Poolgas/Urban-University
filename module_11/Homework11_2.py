8cf035a3fdd418c6115c9db6ec7146511b856d43
Poolgas <Stealik174@gmail.com>
2024-08-23 17:32:53 +0500

    Домашнее задание по теме "Интроспекция"
    Цель задания:
    
    Закрепить знания об интроспекции в Python.
    Создать персональную функции для подробной интроспекции объекта.

---
+++ module_11/Homework11_2.py
@@ -0,0 +1,51 @@
+﻿'''Домашнее задание по теме "Интроспекция"
+Цель задания:
+
+Закрепить знания об интроспекции в Python.
+Создать персональную функции для подробной интроспекции объекта.
+
+Задание:
+Необходимо создать функцию, которая принимает объект (любого типа) в качестве аргумента и проводит интроспекцию этого объекта, чтобы определить его тип, атрибуты, методы, модуль, и другие свойства.
+
+1. Создайте функцию introspection_info(obj), которая принимает объект obj.
+2. Используйте встроенные функции и методы интроспекции Python для получения информации о переданном объекте.
+3. Верните словарь или строки с данными об объекте, включающий следующую информацию:
+  - Тип объекта.
+  - Атрибуты объекта.
+  - Методы объекта.
+  - Модуль, к которому объект принадлежит.
+  - Другие интересные свойства объекта, учитывая его тип (по желанию).
+
+
+Пример работы:
+number_info = introspection_info(42)
+print(number_info)
+
+Вывод на консоль:
+{'type': 'int', 'attributes': [...], 'methods': ['__abs__', '__add__', ...], 'module': '__main__'}
+'''
+def introspection_info(obj):
+    result = {}
+    result['type'] = type(obj).__name__
+    result['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    result['methods'] = [method for method in dir(obj) if callable(getattr(obj, method))]
+    result['module'] = [introspection_info.__module__]
+    return result
+
+class Human:
+    head = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.say_info()
+
+    def say_info(self):
+        print(f'Привет, меня зовут {self.name}, мне {self.age}')
+
+
+number_info1 = introspection_info(42)
+number_info2 = introspection_info(Human('Кирилл', 29))
+
+print(number_info1)
+print(number_info2)
