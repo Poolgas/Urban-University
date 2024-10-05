9add8e3d4f3034cc84a8d67f2ff0874a8cf26d1c
Poolgas <Stealik174@gmail.com>
2024-10-05 22:13:08 +0500

    Update crud_functions.py
    Замена открытия db с .connect на with

--- Homework14_4/crud_functions.py
+++ Homework14_4/crud_functions.py
@@ -1,50 +1,50 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
-cursor = connection.cursor()
+with sqlite3.connect('database.db') as db:
+    cursor = db.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products(
-id INT PRIMARY KEY,
-title TEXT NO NULL,
-description TEXT,
-price INT NOT NULL
-);
-''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INT PRIMARY KEY,
+    title TEXT NO NULL,
+    description TEXT,
+    price INT NOT NULL
+    );
+    ''')
 
-'''
-Create функция которая добавляет в БД продукт по ID если его еще не было в БД
-'''
-
-
-def initiate_db(product_id, title, description, price):
-    check_product = cursor.execute('SELECT * FROM Products WHERE id = ?', (product_id,))
-    if check_product.fetchone() is None:
-        cursor.execute(f'''
-        INSERT INTO Products VALUES('{product_id}', '{title}', '{description}', '{price}')
-        ''')
-    connection.commit()
+    '''
+    Create функция которая добавляет в БД продукт по ID если его еще не было в БД
+    '''
 
 
-'''
-Read функция которая возвращает список словарей созданный с помощью БД
-'''
+    def initiate_db(product_id, title, description, price):
+        check_product = cursor.execute('SELECT * FROM Products WHERE id = ?', (product_id,))
+        if check_product.fetchone() is None:
+            cursor.execute(f'''
+            INSERT INTO Products VALUES('{product_id}', '{title}', '{description}', '{price}')
+            ''')
+        db.commit()
 
 
-def get_all_products():
-    product_list = cursor.execute('SELECT * FROM Products')
-    products = {}
-    for product in product_list:
-        products[product[0]] = product[1], product[2], product[3]
-    connection.commit()
-    return products
+    '''
+    Read функция которая возвращает список словарей созданный с помощью БД
+    '''
 
 
-'''
-Заполнение SQL таблицы
-'''
-for i in range(1, 5):
-    initiate_db(i, f'Продукт {i}', f'Описание {i}', i * 100)
+    def get_all_products():
+        product_list = cursor.execute('SELECT * FROM Products')
+        products = {}
+        for product in product_list:
+            products[product[0]] = product[1], product[2], product[3]
+        db.commit()
+        return products
 
-connection.commit()
+
+    '''
+    Заполнение SQL таблицы
+    '''
+    for i in range(1, 5):
+        initiate_db(i, f'Продукт {i}', f'Описание {i}', i * 100)
+
+    db.commit()
 
